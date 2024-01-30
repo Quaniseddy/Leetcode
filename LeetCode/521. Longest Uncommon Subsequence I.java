08d6c1fd48647eb4487c420cd0b7928f9e672bbf
Quaniseddy <155707840+Quaniseddy@users.noreply.github.com>
2024-01-30 18:29:48 +1100

    Update 521. Longest Uncommon Subsequence I.java

--- LeetCode/521. Longest Uncommon Subsequence I.java
+++ LeetCode/521. Longest Uncommon Subsequence I.java
@@ -30,3 +30,13 @@ Constraints:
 1 <= a.length, b.length <= 100
 a and b consist of lower-case English letters.
 */
+
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+
+        return Math.max(a.length(),b.length());
+    }
+}
